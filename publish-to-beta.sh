1c4d435008c791213f15133e885141811d2251b2
lilou <lilou@coocaa.com>
2021-09-06 10:44:03 +0800

    :wrench: 更新 publish-to-beta.sh

--- publish-to-beta.sh
+++ publish-to-beta.sh
@@ -4,6 +4,6 @@ rm -rf ./target-jar/
 mkdir -p ./target-jar/
 
 cp -f admin/target/ballcat-admin.jar ./target-jar/ballcat-admin.jar
-scp ./target-jar/ballcat-admin.jar root@106.75.185.163://root/w/compose-hub/context/ballcat-admin/app.jar
+scp ./target-jar/ballcat-admin.jar root@ballcat.host://root/w/compose-hub/context/ballcat-admin/app.jar
 
-ssh root@106.75.185.163 "pwd;cd /root/w/compose-hub;sh /root/w/compose-hub/deploy/deploy_ballcat_admin.sh"
+ssh root@ballcat.host "pwd;cd /root/w/compose-hub;sh /root/w/compose-hub/deploy/deploy_ballcat_admin.sh"
